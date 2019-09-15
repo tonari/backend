cc70371ea0afded7ba4b5bb4d4b85bc64ad16754
aticu <15schnic@gmail.com>
2019-09-15 16:52:30 +0200

    Generate hex public key instead of base 64

--- vapid/generate_vapid_signatures.sh
+++ vapid/generate_vapid_signatures.sh
@@ -8,8 +8,7 @@ trap 'die' ERR
 
 PRIVATE_KEY_FILE=${PRIVATE_KEY_FILE:-private.pem}
 PUBLIC_KEY_FILE=${PUBLIC_KEY_FILE:-public.pem}
-PUBLIC_KEY_FILE_BASE64=${PUBLIC_KEY_FILE_BASE64:-public.base64}
-PUBLIC_KEY_FILE_HEX_ARRAY=${PUBLIC_KEY_FILE_HEX_ARRAY:-public.js}
+PUBLIC_KEY_FILE_HEX=${PUBLIC_KEY_FILE_BASE64:-public.hex}
 
 # Generate the VAPID keys according to https://pimeys.github.io/rust-web-push/v0.4.0/web_push/struct.VapidSignatureBuilder.html
 
@@ -19,10 +18,5 @@ openssl ecparam -name prime256v1 -genkey -noout -out $PRIVATE_KEY_FILE
 # Generate the public key
 openssl ec -in $PRIVATE_KEY_FILE -pubout -out $PUBLIC_KEY_FILE 2> /dev/null
 
-# Generate the public key in base64 (for client output)
-openssl ec -in $PRIVATE_KEY_FILE -pubout -outform DER 2> /dev/null | tail -c 65 | base64 | tr '/+' '_-' | tr -d '\n' > $PUBLIC_KEY_FILE_BASE64
-
-# Generate the public key as hexadecimal bytes (for client output)
-echo -n "Uint8Array([" > $PUBLIC_KEY_FILE_HEX_ARRAY
-openssl ec -in $PRIVATE_KEY_FILE -pubout -outform DER 2> /dev/null | tail -c 65 | od -A n -t x1 | tr -d '\n' | sed 's/ /,0x/g' | tail -c +2 >> $PUBLIC_KEY_FILE_HEX_ARRAY
-echo -n "])" >> $PUBLIC_KEY_FILE_HEX_ARRAY
+# Generate the public key in hex (for client output)
+openssl ec -in $PRIVATE_KEY_FILE -pubout -outform DER 2> /dev/null | tail -c 65 | xxd -p | tr -d '\n' > $PUBLIC_KEY_FILE_HEX
